67b878f6d269ae54f2f6830de5a28f7d682cc0bb
Solomon Yunana <solomon.yunana@redacreltd.com>
2023-05-13 23:56:17 +0100

    CLIC-6/created text component

--- fe-libs/ui-library/src/playgrounds/Solomon/index.tsx
+++ fe-libs/ui-library/src/playgrounds/Solomon/index.tsx
@@ -1,6 +1,6 @@
 import { type FC } from 'react'
 import Container from '@mui/material/Container'
-import Rating from '../../libs/components/Rating'
+import Text from '../../libs/components/Text'
 
 const Playground: FC = () => {
     return (
@@ -11,9 +11,25 @@ const Playground: FC = () => {
                 justifyContent: 'space-around',
             }}
         >
-            <Rating value={3.4} totalCount={1200} />
-            <Rating value={5} totalCount={160034345} />
-            <Rating totalCount={16033444045} />
+            <Text color="primary" variant="h3">
+                This works fine H1 Primary
+            </Text>
+            <Text color="secondary" variant="h2">
+                This works fine H2 Secondary
+            </Text>
+            <Text color="error" variant="h4">
+                This works fine H4 Error
+            </Text>
+            <Text color="primary" variant="overline">
+                This works fine primary Overline
+            </Text>
+
+            <Text color="primary" variant="subtitle2">
+                This works fine primary subtitle2
+            </Text>
+            <Text color="primary" variant="subtitle1">
+                This works fine primary subtitle1
+            </Text>
         </Container>
     )
 }
