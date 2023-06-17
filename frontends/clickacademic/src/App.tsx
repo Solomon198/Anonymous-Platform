c5ab49a80286adbcf5e89edc29f213fc510f9eb0
Salihat <105311273+Salihat@users.noreply.github.com>
2023-06-17 10:31:54 +0100

    CLIC-48/Made Adjustment to Newsletter Widget

---
+++ frontends/clickacademic/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Newsletter } from './components'
+
+
+
+function App(): JSX.Element {
+    return (
+        <div className="App">
+            <Newsletter />
+            
+            
+        </div>
+    )
+}
+
+export default App
