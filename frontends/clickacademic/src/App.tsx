e47779a2bd653496f3163a36b7980573eda0cea0
Salihat <105311273+Salihat@users.noreply.github.com>
2023-06-17 13:30:16 +0100

    CLIC-36/Made Adjustment to Newsletter Widget

--- frontends/clickacademic/src/App.tsx
+++ frontends/clickacademic/src/App.tsx
@@ -1,14 +1,29 @@
+import React from 'react'
+import logo from './logo.svg'
 import './App.css'
-import { Newsletter } from './components'
-
-
+import { Text } from '@dixre/ui-library'
+// import { useTranslation } from 'react-i18next'
 
 function App(): JSX.Element {
+    // const { t } = useTranslation()
     return (
         <div className="App">
-            <Newsletter />
-            
-            
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+            <Text variant="h1">Welcome and working</Text>
+            {/* <Text>{t('common:homepage:title')}</Text> */}
         </div>
     )
 }
