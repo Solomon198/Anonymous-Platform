5bb9acd05c1a5745b227bcf256d06de83a126469
Salihat <105311273+Salihat@users.noreply.github.com>
2023-06-21 17:52:54 +0100

    CLIC-39/Created a Navigation Widget

---
+++ frontends/clickacademic/src/components/Navigation/type.ts
@@ -0,0 +1,9 @@
+export enum SelectedPage {
+      Home = "home",
+      Features = "features",
+      Courses = "courses",
+      About = "about",
+      Support = "support",
+      Login = "login",
+      SignUp = "signup"
+}
